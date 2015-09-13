d87e14b9ab585c63cc177070b8f7d7468fcd43d1
Chang Wang <garnwraly@gmail.com>
2015-09-13 04:13:03 -0700

    use table for legend

--- src/graph/legend/legend.js
+++ src/graph/legend/legend.js
@@ -8,13 +8,23 @@ export default Vue.extend({
   },
   template: `
     <div class="legend">
-      <div class="date">{{date | formatDate 'MMMM Do (dddd)'}}</div>
-      <ul class="modules">
-        <li class="module" v-for="module in modules" track-by="name" bind-style="{color: module.color}" on-click="removePackage(module.name)">
-          <div class="nub"></div>
-          {{module.name}}:&nbsp;{{module.downloads.toLocaleString()}}
-        </li>
-      </ul>
+      <table class="modules">
+        <thead class="date">
+          <th>{{date | formatDate 'dddd'}}</th>
+          <th>{{date | formatDate 'MMMM Do'}}</th>
+        </thead>
+        <tbody>
+          <tr class="module" v-for="module in modules" track-by="name" bind-style="{color: module.color}" on-click="removePackage(module.name)">
+            <td class="name-wrapper">
+              <div class="name">
+                <div class="nub"></div>
+                {{module.name}}
+              </div>
+            </td>
+            <td class="downloads">{{module.downloads.toLocaleString()}}</td>
+          </tr>
+        </tbody>
+      </table>
     </div>
   `,
   methods: {
