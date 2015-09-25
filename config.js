3c161b2cd7e2b6a30a54bca9bbf0e97dd60e1c4e
Chang Wang <garnwraly@gmail.com>
2015-09-24 22:19:48 -0400

    minor: fix using wrong ember (should be ember-cli)

--- config.js
+++ config.js
@@ -4,7 +4,7 @@ module.exports = {
     ['browserify', 'webpack'],
     ['node-sass', 'less', 'stylus'],
     ['gulp', 'grunt'],
-    ['backbone', 'react', 'angular', 'ember', 'vue', 'knockout']
+    ['backbone', 'react', 'angular', 'ember-cli', 'vue', 'knockout']
   ],
   palette: ['#2196F3', '#f44336', '#FF9800', '#BBCC24', '#9C27B0', '#795548', '#3F51B5', '#9E9E9E', '#FFEB3B', '##c0392b']
 };
