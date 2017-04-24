8fd47d24f3549c3c5640acd5fd71ef8aa4a5ebf3
Chang Wang <garnwraly@gmail.com>
2017-04-24 13:30:42 -0700

    ✨🐛 handle scoped package names in data fetch

--- src/services/downloads.js
+++ src/services/downloads.js
@@ -2,37 +2,37 @@ import _ from 'lodash';
 import moment from 'moment';
 
 import {packages, setPackages} from '../packages/packages.js'
+import isScopedPackageName from 'is-scoped';
 
 export default (function () {
   const DATE_FORMAT = 'YYYY-MM-DD';
   const end = moment().format(DATE_FORMAT);
   const start = moment().subtract(1, 'year').format('YYYY-MM-DD');
 
-  function allow(entry) {
-    const conditions = [
-      // not a weekend
-      [0, 6].indexOf(entry.day.getDay()) === -1,
-    ];
-    return _.every(conditions);
-  }
-
   function processDownloads(json) {
     return [for (module of (json.package ? [json] : _.values(json)))
       {
         name: module.package,
         // replace '-' with '/' to fix problem with ES5 coercing it to UTC
-        downloads: [
-          for (entry of module.downloads)
-          {day: new Date(entry.day.replace(/\-/g, '/')), count: entry.downloads}
-        ]
+        downloads: module.downloads
+          .map(entry => ({day: new Date(entry.day.replace(/\-/g, '/')), count: entry.downloads}))
       }];
   }
 
-  async function fetchPackages(...packages) {
-    const url = `https://api.npmjs.org/downloads/range/${start}:${end}/${packages.join()}`;
+  async function fetchPackages(...packageNames) {
+    const url = `https://api.npmjs.org/downloads/range/${start}:${end}/${packageNames.join()}`;
     const response = await fetch(url);
     const json = await response.json();
     return processDownloads(json);
+  }
+
+  async function batchFetchPackages(...packageNames) {
+    const { scopedPackageNames, nonScopedPackageNames } = _.groupBy(packageNames, name => isScopedPackageName(name) ? 'scopedPackageNames' : 'nonScopedPackageNames');
+    const [ scopedStats, nonScopedStats ] = [
+      await Promise.all(scopedPackageNames.map((packageName) => fetchPackages(packageName))),
+      await (nonScopedPackageNames && fetchPackages(...nonScopedPackageNames))
+    ];
+    return _.flatten(_.compact(_.concat(scopedStats, nonScopedStats)));
   }
 
   let modules = {};
@@ -53,7 +53,7 @@ export default (function () {
     async fetch(packages, notify=true) {
       setPackages(packages, notify);
 
-      const newData = await fetchPackages(...packages);
+      const newData = await batchFetchPackages(...packages);
       _.forEach(newData, module => {
         modules[module.name] = _.extend({}, modules[module.name], module);
       });
