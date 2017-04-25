6173b1e13e4dc185711111eb76874bab3c820526
Chang Wang <garnwraly@gmail.com>
2017-04-24 21:35:46 -0700

    update preset comparisions

--- config.js
+++ config.js
@@ -1,10 +1,13 @@
 module.exports = {
   presetPackages: [
-    ['lodash', 'underscore'],
     ['browserify', 'webpack'],
+    ['glamor','aphrodite','radium','uranium', 'glamorous'],
+    ['lodash', 'underscore'],
     ['node-sass', 'less', 'stylus'],
     ['gulp', 'grunt'],
-    ['backbone', 'react', 'angular', 'ember-cli', 'vue', 'knockout']
+    ['react', 'angular', '@angular/core', 'ember-cli', 'vue'],
+    ['elm', '@cycle/core'],
+    ['backbone', 'knockout']
   ],
   palette: ['#2196F3', '#f44336', '#FF9800', '#BBCC24', '#9C27B0', '#795548', '#3F51B5', '#9E9E9E', '#FFEB3B', '##c0392b']
 };
