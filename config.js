0ec6072ef0de89088826b873dbedfb836880ed1e
Chang Wang <garnwraly@gmail.com>
2017-04-25 23:43:11 -0700

    add preset packages: recompose, mobx, rxjs, rx, meteor, sails, log4js, winston

--- config.js
+++ config.js
@@ -1,13 +1,17 @@
 module.exports = {
   presetPackages: [
     ['browserify', 'webpack'],
+    ['recompose', 'mobx'],
     ['glamor','aphrodite','radium', 'glamorous'],
     ['lodash', 'underscore'],
     ['node-sass', 'less', 'stylus'],
     ['gulp', 'grunt'],
     ['react', 'angular', '@angular/core', 'ember-cli', 'vue'],
+    ['rxjs', 'rx'],
+    ['log4js', 'winston'],
     ['elm', '@cycle/run'],
-    ['backbone', 'knockout']
+    ['backbone', 'knockout'],
+    ['meteor-node-stubs', 'sails'],
   ],
   palette: ['#2196F3', '#f44336', '#FF9800', '#BBCC24', '#9C27B0', '#795548', '#3F51B5', '#9E9E9E', '#FFEB3B', '##c0392b']
 };
