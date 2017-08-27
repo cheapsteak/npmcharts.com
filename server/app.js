f5ee16d457cf9c144ff8916bdc6804d1b24ec1a6
Chang Wang <garnwraly@gmail.com>
2017-08-26 23:37:14 -0400

    pretty print html if not in production mode

--- server/app.js
+++ server/app.js
@@ -10,6 +10,7 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
+app.locals.pretty = process.env.NODE_ENV !== 'production';
 
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
