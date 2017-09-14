3c68075758da62465510b728d3cc82cb7227a2e0
Chang Wang <garnwraly@gmail.com>
2017-09-14 15:17:03 -0400

    add canonical url

--- server/routes/index.js
+++ server/routes/index.js
@@ -1,29 +1,25 @@
+const url = require('url');
+const querystring = require("querystring");
 const express = require('express');
 const arrayToSentence = require('array-to-sentence');
-const querystring = require("querystring");
 const router = express.Router();
 
 const getTitle = require('../utils/getTitle');
 
-/* GET home page. */
-router.get('/', function(req, res, next) {
-  const fullUrl = req.protocol + '://' + req.get('host') + req.originalUrl;
-  res.render('index', {
-    title: getTitle(),
-    oembedUrl: `https://npmcharts.com/oembed?${querystring.stringify({url: fullUrl, format: 'json'})}`,
-    isEmbed: !!req.query.minimal,
-  });
-});
-
-router.get('/compare/:packages*', function(req, res, next) {
+const sendSPA = function(req, res, next) {
   const packages = req.params.packages.split(',');
-  const fullUrl = req.protocol + '://' + req.get('host') + req.originalUrl;
+  const fullUrl = url.parse(req.protocol + '://' + req.get('host') + req.originalUrl);
   res.render('index', {
     title: getTitle(packages),
-    oembedUrl: `https://npmcharts.com/oembed?${querystring.stringify({url: fullUrl, format: 'json'})}`,
+    oembedUrl: `https://npmcharts.com/oembed?${querystring.stringify({url: fullUrl.href, format: 'json'})}`,
     isEmbed: !!req.query.minimal,
+    canonicalUrl: `https://npmcharts.com${fullUrl.pathname}`,
   });
-});
+}
+
+/* GET home page. */
+router.get('/', sendSPA);
+router.get('/compare/:packages*', sendSPA);
 
 module.exports = router;
 
