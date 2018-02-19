8a051b6e30091381b797d7ea94a39073f03de753
Chang Wang <garnwraly@gmail.com>
2018-02-19 15:25:47 -0500

    eslint and prettier

---
+++ .eslintrc.js
@@ -0,0 +1,12 @@
+module.exports = {
+  "globals": {
+    ga: false,
+  },
+  "extends": ["react-app", "prettier"],
+  "plugins": [
+    "prettier"
+  ],
+  "rules": {
+    "prettier/prettier": "error"
+  }
+}
