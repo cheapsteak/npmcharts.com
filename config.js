3c8e87c5c31826c8c277d7066386557e83b47a1a
Chang Wang <garnwraly@gmail.com>
2018-02-19 14:38:06 -0500

    update preset packages

--- config.js
+++ config.js
@@ -1,16 +1,13 @@
 module.exports = {
   presetPackages: [
-    ['browserify', 'webpack'],
+    ['browserify', 'webpack', 'rollup', 'parcel'],
     ['recompose', 'mobx'],
-    ['glamor','aphrodite','radium', 'glamorous', 'styled-components', 'jss'],
+    ['glamor','aphrodite','radium', 'glamorous', 'styled-components', 'jss', 'emotion'],
     // ['lodash', 'underscore'],
     ['node-sass', 'less', 'stylus'],
     // ['gulp', 'grunt'],
     ['react', 'angular', '@angular/core', 'ember-cli', 'vue'],
-    // ['rxjs', 'rx'],
-    // ['log4js', 'winston'],
-    ['elm', '@cycle/run'],
-    // ['backbone', 'knockout'],
+    ['log4js', 'winston'],
     // ['meteor-node-stubs', 'sails'],
   ],
   palette: ['#2196F3', '#f44336', '#FF9800', '#BBCC24', '#9C27B0', '#795548', '#3F51B5', '#9E9E9E', '#FFEB3B', '##c0392b']
