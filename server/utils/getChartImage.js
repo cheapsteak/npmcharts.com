338cd684c78c4f2b3623ebdb679c4131730b2f92
Chang Wang <garnwraly@gmail.com>
2018-02-24 13:43:09 -0500

    save screenshots and read from saved screenshots

--- server/utils/getChartImage.js
+++ server/utils/getChartImage.js
@@ -1,16 +1,42 @@
-var debug = require('debug')('server:getChartImage');
+const path = require('path');
+const fs = require('fs-extra');
+const debug = require('debug')('server:getChartImage');
+const differenceInWeeks = require('date-fns/difference_in_weeks');
+const sanitizeFilename = require('filenamify');
 const browserPagePool = require('../services/browserPagePool');
+const getPackagesFromUrl = require('./getPackagesFromUrl');
+
+const sanitizeScreenshotFilename = url =>
+  sanitizeFilename(getPackagesFromUrl(url).join(','));
+
+const fileIsNotStale = filePath =>
+  differenceInWeeks(new Date(), Date(fs.statSync(filePath).mtime)) < 3;
 
 module.exports = async url => {
   try {
+    debug('checking if screenshot exists');
+    const screenshotDir = path.join(__dirname, '../../screenshots/');
+    const screenshotPath = path.join(
+      screenshotDir,
+      `${sanitizeScreenshotFilename(url)}.png`,
+    );
+
+    if (fs.existsSync(screenshotPath) && fileIsNotStale(screenshotPath)) {
+      debug('returning existing screenshot');
+      return fs.readFileSync(screenshotPath);
+    }
+
     debug('acquiring page');
     const page = await browserPagePool.acquire();
     debug('going to' + url);
     await page.goto(url, { waitUntil: 'networkidle0', timeout: 60000 });
     debug('setting viewport');
     await page.setViewport({ width: 800, height: 420 });
+    fs.ensureDirSync(screenshotDir);
     debug('taking screenshot');
-    const screenshot = await page.screenshot();
+    const screenshot = await page.screenshot({
+      path: screenshotPath,
+    });
     debug('releasing page');
     await browserPagePool.release(page);
     debug('returning screenshot');
