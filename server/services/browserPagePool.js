649866089b7f2b3f79ce8fda28b3aed78d3eae6b
Chang Wang <garnwraly@gmail.com>
2018-02-24 12:48:57 -0500

    add debug messages for screenshot service

--- server/services/browserPagePool.js
+++ server/services/browserPagePool.js
@@ -1,13 +1,18 @@
+var debug = require('debug')('server:browserPagePool');
 const genericPool = require('generic-pool');
 const puppeteer = require('puppeteer');
 
 const factory = {
   create: async function() {
+    debug('launching browser');
     const browser = await puppeteer.launch();
+    debug('opening new page');
     const page = await browser.newPage();
+    debug('returning page');
     return page;
   },
   destroy: function(puppeteer) {
+    debug('closing browser');
     puppeteer.close();
   },
 };
--- server/utils/getChartImage.js
+++ server/utils/getChartImage.js
@@ -1,10 +1,17 @@
+var debug = require('debug')('server:getChartImage');
 const browserPagePool = require('../services/browserPagePool');
 
 module.exports = async url => {
+  debug('acquiring page');
   const page = await browserPagePool.acquire();
+  debug('going to' + url);
   await page.goto(url, { waitUntil: 'networkidle0', timeout: 60000 });
+  debug('setting viewport');
   await page.setViewport({ width: 800, height: 420 });
+  debug('taking screenshot');
   const screenshot = await page.screenshot();
+  debug('releasing page');
   await browserPagePool.release(page);
+  debug('returning screenshot');
   return screenshot;
 };
