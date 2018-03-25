cbe096551460fe35ea17203ad424beea1a64d517
Chang Wang <garnwraly@gmail.com>
2018-03-24 21:45:17 -0400

    refactor: internalize fetchPackageStats to getPackageStats

--- server/utils/getPackagesStats.js
+++ server/utils/getPackagesStats.js
@@ -1,26 +1,22 @@
 const _ = require('lodash');
 const isScopedPackageName = require('../utils/isScopedPackageName');
 
-function fetchPackagesStats(packageNames, { startDate, endDate }) {
-  const packageNamesParam = packageNames.join(',');
-  const url = `https://api.npmjs.org/downloads/range/${startDate}:${endDate}/${packageNamesParam}`;
-  return fetch(url).then(response => response.json());
-}
-
 async function getPackageStats(packageNames, { startDate, endDate }) {
-  const fetchOptions = { startDate, endDate };
+  function fetchPackagesStats(packageNames) {
+    const packageNamesParam = packageNames.join(',');
+    const url = `https://api.npmjs.org/downloads/range/${startDate}:${endDate}/${packageNamesParam}`;
+    return fetch(url).then(response => response.json());
+  }
+
   const [scopedPackageNames, standardPackageNames] = _.partition(
     packageNames,
     isScopedPackageName,
   );
 
   const scopedPackagesRequests = scopedPackageNames.map(packageName =>
-    fetchPackagesStats([packageName], fetchOptions),
+    fetchPackagesStats([packageName]),
   );
-  const standardPackagesRequest = fetchPackagesStats(
-    standardPackageNames,
-    fetchOptions,
-  );
+  const standardPackagesRequest = fetchPackagesStats(standardPackageNames);
 
   const [
     standardPackagesResponse,
@@ -31,6 +27,7 @@ async function getPackageStats(packageNames, { startDate, endDate }) {
     'package' in standardPackagesResponse
       ? [standardPackagesResponse]
       : Object.values(standardPackagesResponse);
+
   return [...standardPackagesStats, ...scopedPackagesResponse];
 }
 
