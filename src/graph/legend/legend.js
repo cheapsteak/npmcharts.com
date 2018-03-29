e18fc055d0f9caf1753f4b7d5560a061961f210e
Chang Wang <garnwraly@gmail.com>
2018-03-28 22:23:54 -0400

    add week mode to legend

--- src/graph/legend/legend.js
+++ src/graph/legend/legend.js
@@ -1,10 +1,12 @@
 import Vue from 'vue';
 import { removePackage } from '../../packages/packages.js';
+import { isSameMonth, format as formatDate, addDays } from 'date-fns';
 
 export default Vue.extend({
   props: {
     modules: Array,
     date: Date,
+    groupByWeek: Boolean,
   },
   template: require('./legend.html'),
   methods: {
@@ -24,6 +26,16 @@ export default Vue.extend({
     },
     handleMouseLeavePackage(packageName) {
       this.$emit('package-blur', packageName);
+    },
+    formatWeekByStartingDate(startOfWeek) {
+      const startDate = formatDate(startOfWeek, 'MMMM Do');
+      const endOfWeek = addDays(startOfWeek, 6);
+      if (isSameMonth(startOfWeek, endOfWeek)) {
+        const endDate = formatDate(endOfWeek, 'Do');
+        return `${startDate} - ${endDate}`;
+      }
+      const endDate = formatDate(addDays(startOfWeek, 6), 'MMMM Do');
+      return `${startDate} - ${endDate}`;
     },
   },
 });
