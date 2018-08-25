087e935f5208d9a1909346953a13f6fa1632675d
Chang Wang <garnwraly@gmail.com>
2018-08-25 19:53:37 -0400

    switch to module.exports for fetchPacakgeStats

--- packages/utils/stats/fetchPackagesStats.js
+++ packages/utils/stats/fetchPackagesStats.js
@@ -1,6 +1,6 @@
 const fetch = require('isomorphic-fetch');
 
-export default (packageNames, startDate, endDate) => {
+module.exports = (packageNames, startDate, endDate) => {
   const packageNamesParam = packageNames.join(',');
   const url = `https://api.npmjs.org/downloads/range/${startDate}:${endDate}/${packageNamesParam}`;
   return fetch(url).then(response => response.json());
