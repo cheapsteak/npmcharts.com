bca73ff08349b6470ba91ca00468f15ace8389bb
Chang Wang <garnwraly@gmail.com>
2018-11-17 14:04:05 -0500

    Use cachios instead of http-proxy-middleware for npm download endpoint

--- packages/server/routes/api.js
+++ packages/server/routes/api.js
@@ -1,15 +1,23 @@
 const express = require('express');
-const proxy = require('http-proxy-middleware');
+const cachios = require('cachios');
+const LRU = require('lru-cache');
+
+cachios.cache = LRU({
+  max: 500,
+  maxAge: 60 * 1000,
+});
 
 const router = express.Router();
 
-router.use(
-  '/downloads',
-  proxy({
-    target: 'https://api.npmjs.org',
-    changeOrigin: true,
-    pathRewrite: { '^/api': '' },
-  }),
-);
+// Can't use http-proxy-middleware because it triggers
+// Cloudflare's Error 1000 DNS points to prohibited IP
+router.get('/downloads*', async (req, res) => {
+  const proxiedPath = `https://api.npmjs.org${req.path.replace(/^api/, '')}`;
+  try {
+    return res.send((await cachios.get(proxiedPath)).data);
+  } catch (exception) {
+    console.error(exception);
+  }
+});
 
 module.exports = router;
