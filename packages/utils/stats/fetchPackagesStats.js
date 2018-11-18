0af13959b4adcbbd23455dc5a605062d9282f065
Borewit <borewit@users.noreply.github.com>
2018-11-18 21:32:30 +0100

    #76 Fix download from alternative site

--- packages/utils/stats/fetchPackagesStats.js
+++ packages/utils/stats/fetchPackagesStats.js
@@ -2,17 +2,27 @@ const fetch = require('isomorphic-fetch');
 
 module.exports = (packageNames, startDate, endDate) => {
   const packageNamesParam = packageNames.join(',');
-  const url = `https://api.npmjs.org/downloads/range/${startDate}:${endDate}/${packageNamesParam}`;
-  const fallbackUrl = `/api/downloads/range/${startDate}:${endDate}/${packageNamesParam}`;
-  return fetch(url)
-    .then(response => {
-      if (!response.ok) {
-        return fetch(fallbackUrl);
-      }
-      return response.json();
-    })
-    .catch(exception => {
-      console.error(exception);
-      return fetch(fallbackUrl);
-    });
+  const baseUrls = ['https://api.npmjs.org/downloads/range', '/api/downloads/range'];
+
+  function fetchStats() {
+    const baseUrl = baseUrls.shift();
+    if(!baseUrl) {
+      throw new Error('Failed to download chart data.');
+    }
+    const url = `${baseUrl}/${startDate}:${endDate}/${packageNamesParam}`;
+    return fetch(url)
+      .then(response => {
+        if (!response.ok) {
+          return fetch(fallbackUrl);
+        }
+        return response.json();
+      })
+      .catch(err => {
+        console.error(`Failed to download from url=${url}, err: ${err.message}`);
+        return fetchStats(baseUrl); // Try next URL
+      });
+  }
+
+  return fetchStats();
+
 };
