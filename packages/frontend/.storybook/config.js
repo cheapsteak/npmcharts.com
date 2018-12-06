5ef62e8aa3c8e1ac95cd769d16950d8fe2de419c
Chang Wang <garnwraly@gmail.com>
2018-12-05 22:52:19 -0500

    📚Storybook legend

--- packages/frontend/.storybook/config.js
+++ packages/frontend/.storybook/config.js
@@ -1,6 +1,14 @@
+import Vue from 'vue';
 import { configure } from '@storybook/vue';
 
+
+import { format as formatDate } from 'date-fns';
+
 import '../src/style.styl';
+
+Vue.filter('formatDate', function(date, format) {
+  return formatDate(date, format);
+});
 
 // automatically import all files ending in *.stories.js
 const req = require.context('../src', true, /.stories.js$/);
---
+++ packages/frontend/src/graph/legend/legend.stories.js
@@ -0,0 +1,89 @@
+/* eslint-disable react/react-in-jsx-scope, react/no-this-in-sfc */
+
+import { storiesOf } from '@storybook/vue';
+import { action } from '@storybook/addon-actions';
+
+import legend from './legend.js';
+
+const makeLegend = (data) => ({
+  components: { graphLegend: legend },
+  data() {
+    return data;
+  },
+  template: `
+    <div class="chart">
+      <graphLegend
+        :modules="legendData.modules"
+        :interval="interval"
+        :date="legendData.date"
+        @package-focus="handlePackageFocus"
+        @package-blur="handlePackageBlur"
+        @legend-blur="handleLegendBlur"
+        @legend-focus="handleLegendFocus"
+      >
+      </graphLegend>
+    </div>
+  `,
+  methods: {
+    handlePackageFocus: action('packageFocus'),
+    handlePackageBlur: action('packageBlur'),
+    handleLegendFocus: action('legendFocus'),
+    handleLegendBlur: action('legendBlur'),
+  },
+});
+
+storiesOf('legend', module)
+  .add('weekly', () => makeLegend({
+    legendData: {
+      modules: [
+        {
+          color: '#2196F3',
+          downloads: 1606437,
+          name: 'log4js',
+        },
+        {
+          color: '#f44336',
+          downloads: 3228961,
+          name: 'winston',
+        },
+      ],
+      date: new Date(1543467600000),
+    },
+    interval: 7,
+  }))
+  .add('daily', () => makeLegend({
+    legendData: {
+      modules: [
+        {
+          color: '#2196F3',
+          downloads: 310143,
+          name: 'log4js',
+        },
+        {
+          color: '#f44336',
+          downloads: 615741,
+          name: 'winston',
+        },
+      ],
+      date: new Date(1543467600000),
+    },
+    interval: 1,
+  }))
+  .add('monthly', () => makeLegend({
+    legendData: {
+      modules: [
+        {
+          color: '#2196F3',
+          downloads: 6343252,
+          name: 'log4js',
+        },
+        {
+          color: '#f44336',
+          downloads: 13840361,
+          name: 'winston',
+        },
+      ],
+      date: new Date(1543467600000),
+    },
+    interval: 30,
+  }));
