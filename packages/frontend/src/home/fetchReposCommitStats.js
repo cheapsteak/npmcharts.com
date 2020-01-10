36c163624f4dcc09fbd95a7b9754f33070ed386c
Borewit <Borewit@users.noreply.github.com>
2020-01-10 16:13:46 +0100

    Make fetch a bit more error prone (#101)

--- packages/frontend/src/home/fetchReposCommitStats.js
+++ packages/frontend/src/home/fetchReposCommitStats.js
@@ -1,28 +1,28 @@
 import _ from 'lodash';
 import { setDay } from 'date-fns';
 
-export const fetchRepoStats = orgRepo => {
+export async function fetchRepoStats(orgRepo) {
   const url = `https://api.github.com/repos/${orgRepo}/stats/commit_activity`;
-  return fetch(url)
-    .then(response => response.json())
-    .then(data =>
-      _.flow(
-        dataChunkedByWeek =>
-          dataChunkedByWeek.map(week =>
-            week.days.map((commits, i) => ({
-              count: commits,
-              day: setDay(week.week * 1000, i),
-            })),
-          ),
-        _.flatMap,
-      )(data),
-    );
-};
+  const response = await fetch(url);
+  if (response.ok) {
+    const data = response.json();
+    _.flow(
+      dataChunkedByWeek =>
+        dataChunkedByWeek.map(week =>
+          week.days.map((commits, i) => ({
+            count: commits,
+            day: setDay(week.week * 1000, i),
+          })),
+        ),
+      _.flatMap,
+    )(data);
+  }
+}
 
 export const fetchReposStats = orgRepos =>
   Promise.all(
     orgRepos.map(async orgRepo => ({
-      downloads: await fetchRepoStats(orgRepo),
+      downloads: () => fetchRepoStats(orgRepo),
       name: orgRepo,
     })),
   );
