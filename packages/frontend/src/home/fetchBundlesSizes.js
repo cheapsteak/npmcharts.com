6752062f7df1a811df7bbe15450725cb63a77f4b
Jon Lim <jonlimtoronto@gmail.com>
2020-10-08 16:26:13 -0400

    Adding fetch for package bundle size, using bundlephobia (#134)
    * Added a new file, `fetchBundlesSizes.js`, that sends a request to the Bundlephobia api endpoint for fetching sizes of packages
    * Added conditional section to package names displayed in header, to add bundle size if it exists for package after the package name
    * Removed "download trends"
    * Added styling to `<small>`
    * Added `v-if` to `<small>` and removed `<template>`

---
+++ packages/frontend/src/home/fetchBundlesSizes.js
@@ -0,0 +1,30 @@
+async function fetchBundleSize(pkgName) {
+  const url = `https://bundlephobia.com/api/size?package=${pkgName}`;
+  const resp = await fetch(url);
+
+  if (resp.ok) {
+    const data = await resp.json();
+    const parse = input => parseFloat(input).toFixed(1);
+    const format = input => input > 1048576
+      ? `${parse(input / 1048576)} MB` : input > 1024
+        ? `${parse(input / 1024)} KB` : `${input} B`;
+
+    return {
+      gzip: format(data.gzip),
+      size: format(data.size),
+    }
+  }
+};
+
+export const fetchBundlesSizes = pkgNames => {
+  return pkgNames.reduce((acc, pkgName) => {
+    fetchBundleSize(pkgName)
+      .then(bundle => {
+        if (bundle) acc[pkgName] = bundle;
+      });
+    return acc
+  }, {});
+}
+
+
+export default fetchBundlesSizes;
