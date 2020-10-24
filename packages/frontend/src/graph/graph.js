3bc9f9f7f73ccaeb06b07d7320e56eac2b46c450
xiaohanxu <hanxu1996a@gmail.com>
2020-10-24 22:22:17 +0800

    fix: present y-axis in k,m,b unit

--- packages/frontend/src/graph/graph.js
+++ packages/frontend/src/graph/graph.js
@@ -10,19 +10,14 @@ const { palette } = require('configs');
 
 // this can't go in the data of the component, observing it changes it.
 let svg;
-const superscript = '⁰¹²³⁴⁵⁶⁷⁸⁹';
-const formatPower = function(d) {
-  return (d + '')
-    .split('')
-    .map(function(c) {
-      return superscript[c];
-    })
-    .join('');
-};
-const formatLogTick = function(d) {
-  const log = Math.log(d) / Math.LN10;
+const formatLogTick = function(n) {
+  const log = Math.log(n) / Math.LN10;
   if (Math.abs(Math.round(log) - log) < 1e-6) {
-    return 10 + formatPower(Math.round(log));
+    if (n < 1e3) return n;
+    if (n >= 1e3 && n < 1e6) return +(n / 1e3).toFixed(1) + 'K';
+    if (n >= 1e6 && n < 1e9) return +(n / 1e6).toFixed(1) + 'M';
+    if (n >= 1e9 && n < 1e12) return +(n / 1e9).toFixed(1) + 'B';
+    if (n >= 1e12) return +(n / 1e12).toFixed(1) + 'T';
   } else {
     return '';
   }
