0dbf9b661e1c8a95b6ac2dd9df2c6f33572c3f44
Chang Wang <cheapsteak@gmail.com>
2023-04-04 01:16:23 -0400

    checkpoint

--- babel.config.js
+++ babel.config.js
@@ -1,7 +1,8 @@
 module.exports = {
   sourceType: 'unambiguous',
   presets: [
-    '@babel/preset-react',
+    ['@babel/preset-react', { runtime: 'automatic' }],
+    '@babel/preset-typescript',
     [
       '@babel/preset-env',
       {
---
+++ packages/frontend/src/index.tsx
@@ -0,0 +1,40 @@
+import { BrowserRouter, Routes, Route, useParams } from 'react-router-dom';
+import { createRoot } from 'react-dom/client';
+import _ from 'lodash';
+import Vue from 'vue';
+import { format as formatDate } from 'date-fns';
+import config from 'configs';
+
+import { Home } from './home/home';
+import './style.styl';
+// import Home from './home/home.vue';
+
+Vue.filter('formatDate', function(date, format) {
+  return formatDate(date, format);
+});
+
+const root = createRoot(document.getElementById('root'));
+
+const App = () => {
+  const params = useParams();
+  const packageNames = params['*']?.split(',')
+
+  return (
+    <Home
+      packageNames={packageNames ?? _.shuffle(config.presetComparisons)[0]}
+      isMinimalMode={false}
+      isUsingPresetComparisons={packageNames?.length === 0}
+    />
+  );
+};
+
+const RoutedApp = () => (
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />} />
+      <Route path="/compare/*" element={<App />} />
+    </Routes>
+  </BrowserRouter>
+);
+
+root.render(<RoutedApp />);
